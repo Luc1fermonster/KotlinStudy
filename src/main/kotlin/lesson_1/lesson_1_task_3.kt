d707e8111f96e476412ee6a99dce67f863ecc8f8
luc1fermonster <leo.lapaeff@gmail.com>
2024-06-12 09:20:53 +0200

    KS-1-5 done fifth task

--- src/main/kotlin/lesson_1/lesson_1_task_3.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_3.kt
@@ -1,14 +1,18 @@
 package org.example.lesson_1
 
 fun main(){
+
     val year = 1961
-    var hour = 9
-    var minutes = 7
+    var hour:String = "09"
+    var minutes:String = "07"
+
     println(year)
     println(hour)
     println(minutes)
-    hour = 10
-    minutes = 55
+
+    hour = "10"
+    minutes = "55"
+
     println("$hour:$minutes")
 }
 
