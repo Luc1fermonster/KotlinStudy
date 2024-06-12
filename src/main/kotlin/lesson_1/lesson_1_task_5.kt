869583efdc9201f339749eca1cc42baa686de86d
luc1fermonster <leo.lapaeff@gmail.com>
2024-06-12 14:02:26 +0200

    KS-1-5 reworked fifth task

--- src/main/kotlin/lesson_1/lesson_1_task_5.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_5.kt
@@ -1,10 +1,10 @@
 package org.example.lesson_1
 
 fun main(){
-    val seconds = 6_480
+    var seconds = 6_480
     val hours = seconds/3600
     val leftSec1 = seconds-(hours*3600)
     val minutes = leftSec1/60
-    val leftSec2 = seconds - (hours*3600) - (minutes*60)
-    println("$hours:$minutes:$leftSec2")
+    seconds = seconds - (hours*3600) - (minutes*60)
+    println(String.format(("%02d:%02d:%02d"), hours, minutes, seconds))
 }
