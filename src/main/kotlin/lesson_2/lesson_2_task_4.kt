8f0fd50df7c05963ef4122163d8e772e0513b676
luc1fermonster <leo.lapaeff@gmail.com>
2024-06-13 10:08:49 +0200

    KS-2-4 remade fourth task

--- src/main/kotlin/lesson_2/lesson_2_task_4.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_4.kt
@@ -1,12 +1,13 @@
 package org.example.lesson_2
 
+const val DENOMINATOR_FOR_PERCENT = 100
 
 fun main() {
     val crystalOre = 7
     val ironOre = 11
-    val buffMultiplier = 1.2
-    val crystalOreBuffed = crystalOre * buffMultiplier
-    val ironOreBuffed = ironOre * buffMultiplier
+    val buffMultiplier = 20.0
+    val crystalOreBuffed = ((buffMultiplier / DENOMINATOR_FOR_PERCENT) + 1) * crystalOre
+    val ironOreBuffed = ((buffMultiplier / DENOMINATOR_FOR_PERCENT) + 1) * ironOre
     println("Crystal buffed ore ${crystalOreBuffed.toInt()}")
     println("Iron buffed ore ${ironOreBuffed.toInt()}")
 }
