c6f17209ba1cff942bad423cfe862ef14be1882b
luc1fermonster <leo.lapaeff@gmail.com>
2024-06-13 10:20:23 +0200

    KS-2-5 remade fifth task

--- src/main/kotlin/lesson_2/lesson_2_task_5.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_5.kt
@@ -2,10 +2,13 @@ package org.example.lesson_2
 
 import kotlin.math.pow
 
+const val DENOMINATOR_FOR_PERCENT = 100
+
 fun main() {
     val previousValue = 70_000
-    val interestRate = 0.167
+    val interestRate = 16.7
     val duration = 20
-    val futureValue = previousValue * ((1 + interestRate).pow(duration))
+    val futureValue = previousValue * ((1 + (interestRate / DENOMINATOR_FOR_PERCENT)).pow(duration))
+
     println(String.format("%.3f", futureValue))
 }
