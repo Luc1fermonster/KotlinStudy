f37ca3dbb5f2041e938b794ce364cb8ba5c30b7c
luc1fermonster <leo.lapaeff@gmail.com>
2024-06-13 09:54:48 +0200

    KS-2-3 remade second task

--- src/main/kotlin/lesson_2/lesson_2_task_3.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_3.kt
@@ -1,11 +1,14 @@
 package org.example.lesson_2
 
-fun main() {
+const val MIN_IN_HOUR = 60
 
-    val minInHour = 60
+fun main() {
     val minStart = 39
     val hourStart = 9
     val minInTransit = 457
+    val minArrive = minStart + minInTransit
+    val arriveHour = minArrive / MIN_IN_HOUR
+    val remainingMinutes = minArrive % 60
 
-    println(String.format(("%02d:%02d"), (hourStart + ((minInTransit + minStart) / minInHour)), ((minStart + minInTransit) % minInHour)))
+    println(String.format(("%02d:%02d"), (hourStart + arriveHour), remainingMinutes))
 }
