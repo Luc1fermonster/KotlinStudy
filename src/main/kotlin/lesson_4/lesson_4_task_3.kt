985c36899a251e7fcef028c3e12f5eb6182024a3
luc1fermonster <leo.lapaeff@gmail.com>
2024-06-14 22:52:36 +0200

    KS-4-3 fixed third task

--- src/main/kotlin/lesson_4/lesson_4_task_3.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_3.kt
@@ -3,8 +3,10 @@ package org.example.lesson_4
 fun main() {
     val weather = "солнечная"
     val tentCondition = "открыт"
-    val  humidity = 20
+    val humidity = 20
     val season = "зима"
 
-    println("Благоприятность для роста бобовых сейчас: ${(weather == "солнечная") && (tentCondition == "открыт") && (humidity == 20) && (season != "зима")}")
+    val auspiciousness:Boolean = (weather == "солнечная") && (tentCondition == "открыт") && (humidity == 20) && (season != "зима")
+
+    println("Благоприятность для роста бобовых сейчас: $auspiciousness")
 }
