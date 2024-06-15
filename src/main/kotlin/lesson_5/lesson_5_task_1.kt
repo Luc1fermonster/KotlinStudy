0f2ca3bc5cd85cfc4999327a98abc6460cde97b9
luc1fermonster <leo.lapaeff@gmail.com>
2024-06-15 11:56:57 +0200

    KS-5-1 i made numbers random & always different

--- src/main/kotlin/lesson_5/lesson_5_task_1.kt
+++ src/main/kotlin/lesson_5/lesson_5_task_1.kt
@@ -1,8 +1,10 @@
 package org.example.lesson_5
 
+import kotlin.random.Random
+
 fun main() {
-    val numberForBotCheck1 = 9
-    val numberForBotCheck2 = 22
+    val numberForBotCheck1 = Random.nextInt(1, 50)
+    val numberForBotCheck2 = Random.nextInt(1, 50)
     println("""Здравствуйте. Нам нужно понять, что вы не робот
         |Для этого с клавиатуры введите одно число - ответ для примера $numberForBotCheck1 + $numberForBotCheck2: 
     """.trimMargin())
