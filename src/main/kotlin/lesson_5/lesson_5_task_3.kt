496ee774a5a6755919a93f02324c90ca605b9d8a
luc1fermonster <leo.lapaeff@gmail.com>
2024-06-15 11:52:54 +0200

    KS-5-3 i made numbers random

--- src/main/kotlin/lesson_5/lesson_5_task_3.kt
+++ src/main/kotlin/lesson_5/lesson_5_task_3.kt
@@ -1,25 +1,27 @@
 package org.example.lesson_5
 
-const val FIRST_WINNING_NUMBER = 11
-const val SECOND_WINNING_NUMBER = 24
+import kotlin.random.Random
 
 fun main() {
+    val firstWinningNumber = Random.nextInt(1, 43)
+    val secondWinningNumber = Random.nextInt(1, 43)
     println("сделайте первую попытку: ")
     val firstGuess = readln().toInt()
 
     println("сделайте вторую попытку: ")
     val secondGuess = readln().toInt()
 
-    val isSuperPrize = (firstGuess == FIRST_WINNING_NUMBER || firstGuess == SECOND_WINNING_NUMBER) &&
-            (secondGuess == FIRST_WINNING_NUMBER || secondGuess == SECOND_WINNING_NUMBER) &&
+    val isSuperPrize = (firstGuess == firstWinningNumber || firstGuess == secondWinningNumber) &&
+            (secondGuess == firstWinningNumber || secondGuess == secondWinningNumber) &&
             (firstGuess != secondGuess)
 
-    val isConsolationPrize = (firstGuess == FIRST_WINNING_NUMBER || firstGuess == SECOND_WINNING_NUMBER) ||
-            (secondGuess == FIRST_WINNING_NUMBER || secondGuess == SECOND_WINNING_NUMBER)
+    val isConsolationPrize = (firstGuess == firstWinningNumber || firstGuess == secondWinningNumber) ||
+            (secondGuess == firstWinningNumber || secondGuess == secondWinningNumber)
 
     when {
         isSuperPrize -> println("Поздравляем! Вы выиграли главный приз!")
         isConsolationPrize -> println("Вы выиграли утешительный приз!")
         else -> println("В этот раз вам не повезло")
     }
+    println("загаданы были числа $firstWinningNumber и $secondWinningNumber")
 }
