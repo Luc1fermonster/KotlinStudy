643263527cd5e023465b0f89c332e067c58155aa
luc1fermonster <leo.lapaeff@gmail.com>
2024-06-15 11:44:41 +0200

    KS-5-1 a bit fixed first task

--- src/main/kotlin/lesson_5/lesson_5_task_1.kt
+++ src/main/kotlin/lesson_5/lesson_5_task_1.kt
@@ -1,11 +1,13 @@
 package org.example.lesson_5
 
 fun main() {
+    val numberForBotCheck1 = 9
+    val numberForBotCheck2 = 22
     println("""Здравствуйте. Нам нужно понять, что вы не робот
-        |Для этого с клавиатуры введите одно число - ответ для примера 9 + 22: 
+        |Для этого с клавиатуры введите одно число - ответ для примера $numberForBotCheck1 + $numberForBotCheck2: 
     """.trimMargin())
-    val numberRead: String = readln()
-    if (numberRead == "31") {
+    val numberRead = readln().toInt()
+    if (numberRead == (numberForBotCheck2+numberForBotCheck1)) {
         println("Добро пожаловать!")
     }
     else {
