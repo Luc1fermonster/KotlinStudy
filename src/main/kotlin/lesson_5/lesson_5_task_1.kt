52365798e26b47cedc8feba90a7ef5b8ed140992
luc1fermonster <leo.lapaeff@gmail.com>
2024-06-16 05:41:55 +0200

    KS-5-1 little mistake fixed

--- src/main/kotlin/lesson_5/lesson_5_task_1.kt
+++ src/main/kotlin/lesson_5/lesson_5_task_1.kt
@@ -5,14 +5,15 @@ import kotlin.random.Random
 fun main() {
     val numberForBotCheck1 = Random.nextInt(1, 50)
     val numberForBotCheck2 = Random.nextInt(1, 50)
-    println("""Здравствуйте. Нам нужно понять, что вы не робот
+    println(
+        """Здравствуйте. Нам нужно понять, что вы не робот
         |Для этого с клавиатуры введите одно число - ответ для примера $numberForBotCheck1 + $numberForBotCheck2: 
-    """.trimMargin())
+    """.trimMargin()
+    )
     val numberRead = readln().toInt()
-    if (numberRead == (numberForBotCheck2+numberForBotCheck1)) {
+    if (numberRead == (numberForBotCheck2 + numberForBotCheck1)) {
         println("Добро пожаловать!")
-    }
-    else {
+    } else {
         println("Доступ запрещён")
     }
 }
